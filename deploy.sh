2b7f333e0fa98c9dd7d522893a7b943107ed8de5
duongha <friendd1996@gmail.com>
2018-09-09 05:28:53 +0700

    :repeat_one: fix name image kubernetes

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ docker push blademaster996/micro-web-worker:$SHA
 
 kubectl apply -f kubernetes
 
-kubectl set image deployments/client-deployment blademaster996/micro-web-client:$SHA
-kubectl set image deployments/server-deployment blademaster996/micro-web-server:$SHA
-kubectl set image deployments/worker-deployment blademaster996/micro-web-worker:$SHA
+kubectl set image deployments/client-deployment client=blademaster996/micro-web-client:$SHA
+kubectl set image deployments/server-deployment server=blademaster996/micro-web-server:$SHA
+kubectl set image deployments/worker-deployment worker=blademaster996/micro-web-worker:$SHA
